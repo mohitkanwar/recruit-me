9e93db6a0e7c2b178aab168c3bc3f4aabf1f9899
Mohit Kanwar <m.m.kanwar@gmail.com>
2017-07-05 17:15:54 +0530

    project test and quality check setup

---
+++ src/main/java/com/mk/recruitme/RecruitMeApplication.java
@@ -0,0 +1,12 @@
+package com.mk.recruitme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitMeApplication.class, args);
+    }
+}
